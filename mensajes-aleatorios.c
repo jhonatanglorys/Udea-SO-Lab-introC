4763b2349d6efdc68362e9cead9d1005c97a6905
jhonatanglorys <jhonatanglorys@gmail.com>
2020-02-21 16:46:27 -0500

    uso de switch

--- mensajes-aleatorios.c
+++ mensajes-aleatorios.c
@@ -5,19 +5,22 @@
 int generar_aleatorios(int num1, int num2);
 int mayor(int num1, int num2);
 int menor(int num1, int num2);
-
+char mostrarMensaje(int posicion, char arreglo[]);
+char *correctas[4];
+char *incorrectas[4];
 int main(){
-    char *correctas[4];
+    
     correctas[0]="Muy bien!";
     correctas[1]="Excelente!";
     correctas[2]="Buen trabajo!";
     correctas[3]="Sigue haciéndolo bien!";
-    char *incorrectas[4];
+    
     incorrectas[0]="No. Por favor trata de nuevo.";
     incorrectas[1]="Incorrecto. Trata una vez más.";
     incorrectas[2]="No te rindas!";
     incorrectas[3]="No. Trata de nuevo";
     int resultado, intentos, mensaje;
+    char temp;
     intentos=0;
     srand(time(NULL)); //genera la semilla
     int op1, op2, salir;
@@ -27,8 +30,10 @@ int main(){
     printf("¿Cuánto es %d veces %d?", op1, op2);
     scanf("%d", &resultado);
     while(resultado != (op1*op2)){
-        printf(incorrectas[mensaje]);
+        //temp = mostrarMensaje(mensaje,incorrectas);
         mensaje=generar_aleatorios(0,3);
+        printf(mostrarMensaje(mensaje,incorrectas));
+        //printf(incorrectas[mensaje]);
         scanf("%d", &resultado);
         intentos++;
         if (intentos>4){
@@ -39,7 +44,9 @@ int main(){
             }
         }
     }
-    printf(correctas[mensaje]);
+    //temp = mostrarMensaje(mensaje,correctas);
+    printf(mostrarMensaje(mensaje,correctas));
+    //printf(correctas[mensaje]);
     return 0;
 }
 
@@ -66,5 +73,28 @@ int menor(int num1, int num2) {
     }
     else {
         return num2;
+    }
+}
+char mostrarMensaje(int posicion, char arreglo[]){
+    switch (posicion)
+    {
+        case 0:
+        return arreglo[0];
+        break;
+
+        case 1:
+        return arreglo[1];
+        break;
+
+        case 2:
+        return arreglo[2];
+        break;
+
+        case 3:
+        return arreglo[3];
+        break;
+    
+        default:
+            break;
     }
 }
